1c7bceddcb2811e0ced96aaf77fc430b9683da29
Mateusz Zadrużyński <46864876+ThirstyPanda@users.noreply.github.com>
2021-05-10 21:08:16 +0200

    Szkielet aplikacji

--- cypress/integration/buying.spec.js
+++ cypress/integration/buying.spec.js
@@ -1,10 +1,28 @@
 describe('Zakup', () => {
-    it('Ładowawnie strony głównej', () => {
+    it('1.Ładowawnie strony głównej', () => {
       cy.visit('http://localhost:3000/');
 });
 
-it('Otwieranie sklepu', () => {
-    cy.get('.span').contains('SHOP');
+it('2.Otwieranie strony sklepu', () => {
+    cy.contains('SHOP').click()
+  });
+
+
+  it('3.Wybieranie produktu z listy ', () => {
+    cy.contains('Test').click()
+  });
+
+  it('4.Wybieranie wielkości produktu ', () => {
+    cy.get('#size').select('LARGE')
+  });
+
+  it('5.Wybieranie ilości zamówienia ', () => {
+    cy.get('#quantity').clear() 
+    cy.get('#quantity').type('3')
+  });
+
+  it('6.Dodawane zamównienia do koszka ', () => {
+    cy.contains('ADD TO CART').click()
   });
 
 });
