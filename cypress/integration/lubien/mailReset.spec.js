f1e3e9f40eba7a8b2606f96bfaf2a092978049db
Mateusz Zadrużyński <46864876+ThirstyPanda@users.noreply.github.com>
2021-05-21 20:04:15 +0200

    Mail reset test

--- cypress/integration/lubien/mailReset.spec.js
+++ cypress/integration/lubien/mailReset.spec.js
@@ -1,17 +1,54 @@
-describe('Dodawanie produktu do koszuka', () => {
-it('1.Przejście do logowania', () => {
-    cy.wait(500)
-    cy.contains('MY ACCOUNT').click()
-  });
+describe('Resetowanie hasła', () => {
+  it('Przejście do logowania', () => {
+    cy.visit('http://localhost:3000/my-account');
+    });
+  
+    it('Resetowanie hasła ', () => {
+      cy.contains('Reset your password').click()
+    });
+  
+    it('Wpisywanie maila', () => {
+      cy.wait(500)
+      cy.get('input[name="email"]').type('testertestuje@int.pl') 
+      cy.contains('Continue').click()
+    })
 
-  it('2.Resetowanie hasła ', () => {
+  it('Przejście do strony z pocztą', () => {
     cy.wait(500)
-    cy.get('input[name="email"]').type('testertestuje@int.pl') 
-    cy.wait(500)
-    cy.get('input[name="password"]').type('1231232323123123')
-    cy.wait(500)
-    cy.contains('Continue').click()
-    cy.contains('Reset your password').click()
-  });
+    cy.visit('https://int.pl/#/login-clear');
+  })
 
-});
+  it('Logowanie na maila', () => {
+    cy.wait(500)
+    cy.get('input[id="emailId"]').type('testertestuje@int.pl') 
+    cy.get('input[id="passwordId"]').type('zaq1@WSX') 
+    cy.contains('loguję się').click()
+    cy.wait(1000)
+
+    })
+    
+    it('Pobieranie linku', () =>{
+      const getIframeDocument = () => {
+        return cy
+        .get('iframe[nxt-message-iframe-data="message"]')
+        .its('0.contentDocument').should('exist')
+      }
+  
+      const getIframeBody = () => {
+        // pobieranie dokumentu
+        return getIframeDocument()
+        // srawdzenie cz dokument się załadował
+        .its('body').should('not.be.undefined')
+        // dostęp do elementów iframe
+        .then(cy.wrap)
+      }
+      getIframeBody().find('a').click({ multiple: true })
+      
+      getIframeBody().find('a', 'get').then(($el) => {
+        const link = $el.attr('href')
+        cy.visit(link)
+      })
+  
+    })
+  
+});  
